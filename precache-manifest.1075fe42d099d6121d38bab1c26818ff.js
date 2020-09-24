fe8f71c1f7bb199357e56aa3882e221435e79a85
Ivan <ivanfistrek5@gmail.com>
2020-09-24 12:19:20 +0200

    Updates

---
+++ precache-manifest.1075fe42d099d6121d38bab1c26818ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4729f2ff34fbe4b96e8584d5c26abf24",
+    "url": "/React-API/index.html"
+  },
+  {
+    "revision": "6685b96298bf18d2b978",
+    "url": "/React-API/static/css/main.953c9007.chunk.css"
+  },
+  {
+    "revision": "cca55e8e0634ff2b5146",
+    "url": "/React-API/static/js/2.6bf0154d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-API/static/js/2.6bf0154d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6685b96298bf18d2b978",
+    "url": "/React-API/static/js/main.8c3c839d.chunk.js"
+  },
+  {
+    "revision": "623c65326ae40648f422",
+    "url": "/React-API/static/js/runtime-main.da7e2fc5.js"
+  }
+]);
