281a2f7169b9227607fb6c89327a8a4e23b9d9c3
Ivan <ivanfistrek5@gmail.com>
2020-10-06 11:19:07 +0200

    Master: Added a fallback function for video rendering

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Contacts from './components/contacts'
+import './App.css';
 
     class App extends Component {
 
--- src/components/contacts.js
+++ src/components/contacts.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
 const Contacts = ({ contacts }) => {
+
+    const renderAPODContent = () => {
+        if(contacts.media_type == 'image') {
+            return <img width="450" className="img-responsive" src = {contacts.hdurl}></img>;
+        }
+        if (contacts.media_type == 'video') {
+            return <iframe className="embed-responsive-item" width="640" height="360" src={contacts.url}></iframe>
+        }
+    }
+
     return (
       <div>
         <center>
             <h1>Astronomy Picture of the Day</h1>
+            <h2>{new Date().toLocaleTimeString()}</h2>
             <p><b>{contacts.date}</b></p>
             </center>
             <div className="container">
@@ -13,7 +24,7 @@ const Contacts = ({ contacts }) => {
                         <p>{contacts.explanation}</p>
                     </div>
                     <div className="col">
-                        <img width="450" className="img-responsive" src = {contacts.hdurl}></img>
+                        {renderAPODContent()}
                     </div>
                 </div>
             </div>
